68fe9ea953a241629dcf6f701bbb6263f95a9953
Roman Tolstosheyev <izobretatel777@mail.ru>
2022-11-16 09:32:24 +0400

    CWriting to files class activity

--- Lesson 9/Class Activity/app.js
+++ Lesson 9/Class Activity/app.js
@@ -1,5 +1,6 @@
 const https = require('https');
 const fs = require('fs');
+const http = require('http');
 
 
 const url = 'https://jsonplaceholder.typicode.com/users';
@@ -21,8 +22,25 @@ https.get(url, (res) => {
                 if (err) {
                     console.log(err);
                 }
-                console.log(data);
+                http.createServer((req, res) => {
+                    fs.readFile('users.json', (err, data) => {
+                        res.writeHead(200, { 'Content-Type': 'text/html' });
+                        data = JSON.parse(data);
+                        let table = '<table border="1"> <tr> <th>id</th> <th>name</th> <th>username</th> </tr>';
+                        for (let i = 0; i < data.length; i++) {
+                            table += '<tr>';
+                            table += '<td>' + data[i].id + '</td>';
+                            table += '<td>' + data[i].name + '</td>';
+                            table += '<td>' + data[i].username + '</td>';
+                            table += '</tr>';
+                        }
+                        res.write(table);
+                        return res.end();
+                    });
+                }
+                ).listen(8081);  
             });
+            
         });
     });
 }
